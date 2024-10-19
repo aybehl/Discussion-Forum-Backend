c969f2d6700cea3801e8123837b8bd0456ec535d
Ayushi Behl <ayushi.behl94@gmail.com>
2024-10-19 17:51:11 -0400

    first commit

---
+++ src/main/java/com/forum/discussion_platform/DiscussionPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.forum.discussion_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscussionPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscussionPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forum/discussion_platform/DiscussionPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forum.discussion_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscussionPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
