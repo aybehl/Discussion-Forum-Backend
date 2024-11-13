7273e8d650ca96790f14d866711780fb10b44fd7
Ayushi Behl <ayushi.behl94@gmail.com>
2024-11-13 10:59:58 -0500

    Added a file for enabling CORS requests

---
+++ src/main/java/com/forum/discussion_platform/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.forum.discussion_platform.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Apply CORS to all endpoints
+                .allowedOrigins("http://localhost:5173") // Allow requests from your frontend origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
+                .allowedHeaders("*") // Allow all headers
+                .allowCredentials(true); // Allow credentials if needed
+    }
+}
--- src/main/java/com/forum/discussion_platform/constants/GenericConstants.java
+++ src/main/java/com/forum/discussion_platform/constants/GenericConstants.java
@@ -3,7 +3,7 @@ package com.forum.discussion_platform.constants;
 public class GenericConstants {
     public static final String USER_NOT_FOUND = "User not found";
 
-    public static final String USER_NOT_FOUND_BY_EMAIL = "User not found by email";
+    public static final String USER_NOT_FOUND_BY_EMAIL = "User not found by email. Try signing up with this email";
     public static final String USER_REGISTRATION_SUCCESSFUL = "User registration successful";
     public static final String USER_LOGIN_SUCCESSFUL = "User login successful";
 
@@ -16,7 +16,7 @@ public class GenericConstants {
     public static final String UNAUTHORISED_DELETE_USER_ACTION = "Unauthorised delete user action";
 
     public static final String INVALID_PASSWORD = "Invalid password";
-    public static final String EMAIL_ALREADY_IN_USE = "Email already in use";
+    public static final String EMAIL_ALREADY_IN_USE = "Email already in use. Please try again with a different email";
     public static final String USER_DETAILS_MISMATCH = "User data mismatch";
 
     public static final String QUESTION_NOT_FOUND = "Question not found";
