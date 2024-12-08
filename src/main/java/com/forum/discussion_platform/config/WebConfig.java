b4876aefbb92e3cc8d6c3ecab77e4f21e08c2ac2
Ayushi Behl <ayushi.behl94@gmail.com>
2024-12-08 11:45:36 -0500

    Resolving CORS issue for render frontend URL

--- src/main/java/com/forum/discussion_platform/config/WebConfig.java
+++ src/main/java/com/forum/discussion_platform/config/WebConfig.java
@@ -11,7 +11,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply CORS to all endpoints
-                .allowedOrigins("http://localhost:5173") // Allow requests from your frontend origin
+                .allowedOrigins("http://localhost:5173",
+                        "https://discussion-forum-frontend.onrender.com"
+                ) // Allow requests from your frontend origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow credentials if needed
